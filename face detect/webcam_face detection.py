22a0237caaed4f76d0c375884226f5addee27582
Albin Baby <albinbabayme05@gmail.com>
2025-02-11 22:58:23 +0100

    Add FPS calculation and overlay on video

--- face detect/webcam_face detection.py
+++ face detect/webcam_face detection.py
@@ -1,4 +1,5 @@
 import cv2
+import time
 
 # Load the pre-trained face detection model
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -11,12 +12,21 @@ cam.set(3, 640)  # Width
 cam.set(4, 480)  # Height
 cam.set(10, 100) # Brightness
 
+start_time = 0
 while True:
     ret, frame = cam.read()
     faces = face_cascade.detectMultiScale(frame, 1.1, 4)
 
+    # Calculate FPS
+    current_time = time.time()
+    fps = 1 / (current_time - start_time)
+    start_time = current_time
+
     for (x, y, width, height) in faces:
         cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    # Display FPS on the video feed
+    cv2.putText(frame, f"FPS: {int(fps)}", (520, 40), cv2.FONT_HERSHEY_PLAIN, 1.1, (255, 255, 0), 3)
 
     cv2.imshow("Video", frame)
 
