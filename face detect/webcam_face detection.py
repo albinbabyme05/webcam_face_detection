aefb1fba496f7028bdf027ca28c2f52b26e4e695
Albin Baby <albinbabayme05@gmail.com>
2025-02-11 22:57:47 +0100

    Implement face detection using Haar Cascade

--- face detect/webcam_face detection.py
+++ face detect/webcam_face detection.py
@@ -1,10 +1,3 @@
-
-import cv2
-
-# Load the pre-trained face detection model
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
 import cv2
 
 # Load the pre-trained face detection model
@@ -17,3 +10,19 @@ cam = cv2.VideoCapture(0)
 cam.set(3, 640)  # Width
 cam.set(4, 480)  # Height
 cam.set(10, 100) # Brightness
+
+while True:
+    ret, frame = cam.read()
+    faces = face_cascade.detectMultiScale(frame, 1.1, 4)
+
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+    cv2.imshow("Video", frame)
+
+    if cv2.waitKey(10) % 256 == 27:  # Escape key to exit
+        print('Escape pressed. Closing the application')
+        break
+
+cam.release()
+cv2.destroyAllWindows()
